be2fa5eac1f25f8227f00ccd2988879dde395bb9
Cat <catherineyangli@outlook.com>
2019-01-20 18:45:09 -0800

    Starting on rock paper scissors

--- src/components/RockPaperScissors.js
+++ src/components/RockPaperScissors.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import StopWatch from'./components/StopWatch';
 
 class App extends Component {
   render() {
--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -6,53 +6,75 @@ class StopWatch extends Component {
     super(props);
     this.state = {
       time: Date(Date.now()).toString(),
+      seconds: 0,
       runTimer: true,
       isPaused: false
     }
-
+    this.timeInterval = null;
     this.handlePause = this.handlePause.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
 
-  // loopDate = setInterval(() => {
-  //   this.setState({
-  //     time: Date(Date.now()).toString()
-  //   })
-  // }, 1000);
-
   loopTime = function(){
-
+    // let secondsElapsed = ++this.state.seconds;
     var timer = setInterval(() => {
+    let secondsElapsed = this.state.seconds;
+      ++secondsElapsed;
       this.setState({
-        time: Date(Date.now()).toString()
+        seconds: secondsElapsed
       })
     }, 1000);
-    
+
     if( ! this.state.runTimer) {
         clearInterval(timer);
     }
   } 
 
   componentDidMount(){
-    // setInterval(() => {
-    //   this.setState({
-    //     time: Date(Date.now()).toString()
-    //   })
-    // }, 1000)
+    this.startTimer();
+  }
 
-    this.loopTime();
+  startTimer(){
+    this.timeInterval = setInterval(() => {
+        if(! this.state.isPaused) {
+          let secondsElapsed = this.state.seconds;
+          ++secondsElapsed;
+          this.setState({
+            seconds: secondsElapsed
+          });
+        };
+      }, 1000);
   }
 
   handlePause(){
-    this.setState({
-      runTimer: false
-    })
+    // var currentTime = this.state.seconds;
+    clearInterval(this.timeInterval);
+    //If timer is running
+    if( this.state.isPaused) {
+        this.startTimer();
+    }
+
+    //toggle isPaused
+    this.setState(state => ({
+      isPaused: !state.isPaused
+    }));
+
   }
 
+  handleReset(){
+    clearInterval(this.timeInterval);
+    this.setState({
+      seconds: 0,
+      isPaused: true
+    })
+  }
   render() {
     return (
       <div className="">
-        <p>The current time is: {this.state.time}</p>
-        <button onClick={this.handlePause}>Pause</button>
+        <p>The current time is: {this.state.seconds}</p>
+        <button onClick={this.handlePause}>{ this.state.isPaused ? "Restart" : "Pause" }</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
